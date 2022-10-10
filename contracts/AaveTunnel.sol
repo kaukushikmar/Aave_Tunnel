69f1232812443cc5f927a28a88234a0774bb1ee0
kaukushikmar <itsmekumar.kaushik@gmail.com>
2022-10-10 10:10:04 +0530

    supply logic for wallet

--- contracts/AaveTunnel.sol
+++ contracts/AaveTunnel.sol
@@ -1,64 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./interfaces/ILendingPoolAddressProvider.sol";
-import "./interfaces/ILendingPool.sol";
-import "./interfaces/IWeth.sol";
-import "./interfaces/IERC20.sol";
-import { IAToken } from "./interfaces/IAToken.sol";
-import { IPriceOracle } from "./interfaces/IPriceOracle.sol";
-import { ValidationLogic } from "./logic/ValidationLogic.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import { Error } from "./helper/Error.sol";
-import "hardhat/console.sol";
+import "./Wallet.sol";
+
+interface IWallet {
+  function supply(address _asset, uint256 _amount) external;
+}
 
 /** @notice user can only supply weth and borrow weth */
 contract AaveTunnel {
-  using SafeMath for uint256;
+  address immutable _walletLogic;
+  uint256 _check = 1;
+  mapping(address => bool) isSupplied;
+  mapping(address => address) userToWallet;
 
-  /** State Variable */
-  uint256 _check = 1; // reentrancy check
-  mapping(address => uint256) AmountSupplied;
-  mapping(address => uint256) AmountBorrowed;
-
-  ILendingPool immutable lendingPool;
-  IAToken immutable aTokens;
-  IWeth immutable weth;
-  IPriceOracle immutable priceOracle;
-
-  constructor(
-    address _lendingPool,
-    address _aTokens,
-    address _weth,
-    address _oracle
-  ) {
-    lendingPool = ILendingPool(_lendingPool);
-    aTokens = IAToken(_aTokens);
-    weth = IWeth(_weth);
-    priceOracle = IPriceOracle(_oracle);
+  constructor(address _add) {
+    _walletLogic = _add;
   }
 
   function supply(address _asset, uint256 _amount) external {
-    IERC20 token = IERC20(_asset);
-
-    require(token.balanceOf(msg.sender) >= _amount, "Insufficient balance");
-    console.log("Passed the insufficient balance check");
-    require(
-      token.transferFrom(msg.sender, address(this), _amount),
-      "Transfer failed"
-    );
-
-    uint256 initialPoolBalance = aTokens.balanceOf(address(this));
-    console.log(initialPoolBalance);
-
-    token.approve(address(lendingPool), _amount);
-    lendingPool.deposit(_asset, _amount, address(this), 0);
-
-    uint256 finalPoolBalance = aTokens.balanceOf(address(this));
-    console.log(finalPoolBalance);
-    AmountSupplied[msg.sender] = AmountSupplied[msg.sender].add(
-      finalPoolBalance.sub(initialPoolBalance)
-    );
+    if (isSupplied[msg.sender] == true) {
+      IWallet wallet = IWallet(userToWallet[msg.sender]);
+      wallet.supply(_asset, _amount);
+    } else {
+      Wallet wallet = new Wallet(_walletLogic);
+      isSupplied[msg.sender] = true;
+      userToWallet[msg.sender] = address(wallet);
+      wallet.supply(_asset, _amount);
+    }
   }
 
   // function supplyEth() external payable {
